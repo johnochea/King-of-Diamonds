53b3d37e7c7c90e7fa49f2c90c6a973e88778efe
johnochea <johnoochea@gmail.com>
2023-02-06 21:31:16 +0800

    intro screen created

--- king_of_diamonds/lib/main.dart
+++ king_of_diamonds/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/basic.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +16,48 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'King of Diamonds',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.black54,
         primarySwatch: Colors.grey,
       ),
-      home: const MyHomePage(title: 'Balance Scale'),
+      home: const IntroPage(),
+    );
+  }
+}
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            const Icon(Icons.scale, size: 30, color: Color.fromARGB(255, 135, 0, 0),),
+            const SizedBox(height: 15,),
+            const Text(
+              'Balance Scale',
+              style: TextStyle(color: Colors.white, fontSize: 45),
+                ),
+            const SizedBox(height: 5,),
+            const Text(
+              'Difficulty: King of Diamonds',
+              style: TextStyle(color: Color.fromARGB(100, 200, 200, 200), fontSize: 18),
+            ),
+
+            const SizedBox(height: 100,),
+            TextButton(onPressed: () {print("heh");}, child: Text('Play', style: TextStyle(color: Colors.white, fontSize: 25),), style: TextButton.styleFrom(backgroundColor: Color.fromARGB(255, 135, 0, 0),fixedSize: Size.fromWidth(175),),)
+          ],
+        ), 
+      ),
+    
     );
   }
 }
@@ -34,6 +76,7 @@ class _MyHomePageState extends State<MyHomePage> {
   int _loses = 0;
   int _myNumber = 0;
   var num_list = [0,0,0,0,0,0,0,0];
+  var scores = [0,0,0,0,0,0,0,0];
 
   void _setWins() {
     setState(() {
@@ -61,45 +104,30 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
+      // appBar: AppBar(
+      //   // Here we take the value from the MyHomePage object that was created by
+      //   // the App.build method, and use it to set our appbar title.
+      //   // title: Text(widget.title),
+      // ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_wins',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            // const Text(
+            //   'You have pushed the button this many times:',
+            // ),
+            // Text(
+            //   '$_wins',
+            //   style: Theme.of(context).textTheme.headlineMedium,
+            // ),
+            const Text('hehe'),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _setWins,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.person),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
